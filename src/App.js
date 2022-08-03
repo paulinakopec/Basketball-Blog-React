5a7674faaf1dc62c388129c19d2ef16eac2250f2
paulinakopec <paulinakopec32@gmail.com>
2022-08-03 15:14:18 +0200

    React router linking in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// components
 import Topbar from "./components/topbar/Topbar";
-// import Home from "./pages/home/Home";
 import Newsletter from "./components/newsletter/Newsletter";
 import Footer from "./components/footer/Footer";
-// import Single from "./pages/single/Single";
-// import AboutMe from "./pages/aboutme/AboutMe";
+
+// pages
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import AboutMe from "./pages/aboutme/AboutMe";
 import ContactPage from "./pages/contact/ContactPage";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+{/* topbar is a component visible on each page */}
       <Topbar></Topbar>
-      {/* <Home></Home> */}
-      {/* <Single></Single> */}
-      {/* <AboutMe></AboutMe> */}
-      <ContactPage></ContactPage>
+
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+      </Routes>
+      <Routes>
+        <Route path="/contact" element={<ContactPage/>} />
+      </Routes>
+      <Routes>
+        <Route path="/about" element={<AboutMe/>} />
+      </Routes>
+      <Routes>
+        <Route path="/news/billrussell" element={<Single/>} />
+      </Routes>
+
+{/* newsletter and footer are components visible on each page */}
       <Newsletter></Newsletter>
       <Footer></Footer>
-    </>
+
+    </BrowserRouter>
   );
 }
 
