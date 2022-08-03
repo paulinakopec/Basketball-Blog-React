00e063237eaa2daa89a5199e9e8e48de5dd8ba23
paulinakopec <paulinakopec32@gmail.com>
2022-08-03 12:08:17 +0200

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Topbar from "./components/topbar/Topbar";
-import Home from "./pages/home/Home";
+// import Home from "./pages/home/Home";
 import Newsletter from "./components/newsletter/Newsletter";
 import Footer from "./components/footer/Footer";
-// import Single from "./pages/single/Single";
+import Single from "./pages/single/Single";
 // import AboutMe from "./pages/aboutme/AboutMe";
 
 function App() {
   return (
     <>
       <Topbar></Topbar>
-      <Home></Home>
-      {/* <Single></Single> */}
+      {/* <Home></Home> */}
+      <Single></Single>
       {/* <AboutMe></AboutMe> */}
       <Newsletter></Newsletter>
       <Footer></Footer>
