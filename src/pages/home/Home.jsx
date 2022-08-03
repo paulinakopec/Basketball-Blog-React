76e1b3ed42898415e4ad792d25196359b0bc2bf4
paulinakopec <paulinakopec32@gmail.com>
2022-08-03 12:04:20 +0200

    Deleting sidebar from homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../../components/header/Header';
 import Posts from '../../components/posts/Posts';
-import Sidebar from '../../components/sidebar/Sidebar';
 import '../home/home.css';
 
 const Home = () => {
