97b529480ca9344dde5260dbebf074cdb8066ad4
paulinakopec <paulinakopec32@gmail.com>
2022-08-03 13:23:32 +0200

    Adding elements and styling to about section

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Topbar from "./components/topbar/Topbar";
 // import Home from "./pages/home/Home";
 import Newsletter from "./components/newsletter/Newsletter";
 import Footer from "./components/footer/Footer";
-import Single from "./pages/single/Single";
-// import AboutMe from "./pages/aboutme/AboutMe";
+// import Single from "./pages/single/Single";
+import AboutMe from "./pages/aboutme/AboutMe";
 
 function App() {
   return (
     <>
       <Topbar></Topbar>
       {/* <Home></Home> */}
-      <Single></Single>
-      {/* <AboutMe></AboutMe> */}
+      {/* <Single></Single> */}
+      <AboutMe></AboutMe>
       <Newsletter></Newsletter>
       <Footer></Footer>
     </>
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import '../about/about.css';
 import Me from '../../img/me.jpg';
+import Me2 from '../../img/me2.JPG';
+import Me3 from '../../img/me3.JPG';
+import Me4 from '../../img/me4.jpg';
+
 
 const About = () => {
   return (
     <div className="about">
-        <p className="aboutDescription">
-            Hi! My name is Paulina and I am the author of this blog, which was created due to my long-time love to basketball. 
-            I am a basketball player recently playing in DPD in an amateur basketball league called "Koszykarska Liga Biznesu" in Warsaw
-            and streetball on indoor and outdoor courts.
-            In the past, I was part of AZS SGH Basketball Team, Chicago Bulls Team in Ladies League, AKS Zły and La Basket Warsaw.
-        </p>
-        <img src={Me} alt="" className="aboutImg"/>
+        <div className="descriptionContainer">
+          <p className="aboutDescription">
+              Hi! My name is Paulina and I am the author of this blog, which was created due to my long-time love to basketball. 
+              <br></br><br></br>I am a basketball player recently playing in DPD team in an amateur basketball league called "Koszykarska Liga Biznesu" in Warsaw
+              and streetball on indoor and outdoor courts. In the past, I was part of AZS SGH Basketball Team, Chicago Bulls Team in Ladies League, AKS Zły and La Basket Warsaw.
+              <br></br><br></br>In this blog, I focus mainly on NBA news, but also what's going on in Europe - mainly Eurobasket - and in Poland - PZKosz.
+          </p>
+        </div>
+        <div className="imgContainer">
+          <img src={Me} alt="" className="aboutImg"/>
+          <img src={Me2} alt="" className="aboutImg"/>
+          <img src={Me3} alt="" className="aboutImg"/>
+          <img src={Me4} alt="" className="aboutImg"/>
+        </div>
         
     </div>
   )
