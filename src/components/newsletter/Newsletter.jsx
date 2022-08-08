f82a200e41a77585d7fd4d4f3a501abd4c18fa52
paulinakopec <paulinakopec32@gmail.com>
2022-08-08 11:50:24 +0200

    Adding useState hook to newsletter button

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../newsletter/newsletter.css';
 
 const Newsletter = () => {
+  const [ button, setButton ] = useState('SIGN UP');
+  const changeMessage = () => {
+    setButton('SIGNED UP');
+  }
+
   return (
     <div className="newsletter">
         <h2 className="newsletterTitle">NEWSLETTER</h2>
         <p className="newsletterSubtitle">Sign up to receive daily basketball news.</p>
         <div className="emailInput">
             <input type="text" placeholder="Your email..." className="newsletterInput" autoFocus={true}/>
-            <button className="signUpButton">SIGN UP</button>
+            <button className="signUpButton" onClick={changeMessage}>{button}</button>
         </div>
     </div>
   )
