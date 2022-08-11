8b81cd74ecc8b7bcd51ae779f619fe7235db59cf
paulinakopec <paulinakopec32@gmail.com>
2022-08-11 11:42:19 +0200

    Adding controlled input to form

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -2,9 +2,18 @@ import React, { useState } from 'react';
 import '../newsletter/newsletter.css';
 
 const Newsletter = () => {
+  const [ emailaddress, setEmailAddress ] = useState('');
   const [ button, setButton ] = useState('SIGN UP');
-  const changeMessage = () => {
-    setButton('SIGNED UP');
+
+  const changeMessage = (e) => {
+    e.preventDefault();
+    if (emailaddress) {
+      setButton('SIGNED UP');
+      setEmailAddress('');
+      setTimeout (() => setButton('SIGN UP'), 2000);
+    } else {
+      alert('Please fill in the input');
+    }
   }
 
   return (
@@ -12,7 +21,17 @@ const Newsletter = () => {
         <h2 className="newsletterTitle">NEWSLETTER</h2>
         <p className="newsletterSubtitle">Sign up to receive daily basketball news.</p>
         <div className="emailInput">
-            <input type="text" placeholder="Your email..." className="newsletterInput" autoFocus={true}/>
+            <input 
+              type="text"
+              name="emailaddress"
+              id="emailaddress"
+              value={emailaddress} 
+              placeholder="Your email..." 
+              className="newsletterInput" 
+              autoFocus={true} 
+              onChange={(e) => setEmailAddress(e.target.value)}
+              required
+            />
             <button className="signUpButton" onClick={changeMessage}>{button}</button>
         </div>
     </div>
