d648f386e73d502b987b4fd6865ace355fead859
paulinakopec <paulinakopec32@gmail.com>
2022-08-16 14:31:57 +0200

    Adding onClick

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -3,15 +3,18 @@ import './categories.css';
 import { categoryItems } from '../../data';
 
 const Categories = () => {
-  return (
-    <div className='categories'>
-        {categoryItems.map((category) => (
-            <div className='category' key={category.id}>
-                <img className='categoryImg' src={category.img} alt=''></img>
-                <h2 className='categoryTitle'>{category.title}</h2>
-            </div>
-        ))}
-    </div>
+    const clickHandler = () => {
+        alert('Still working on it...')
+    }
+    return (
+        <div className='categories'>
+            {categoryItems.map((category) => (
+                <div className='category' key={category.id} onClick={clickHandler}>
+                    <img className='categoryImg' src={category.img} alt=''></img>
+                    <h2 className='categoryTitle'>{category.title}</h2>
+                </div>
+            ))}
+        </div>
   )
 }
 
